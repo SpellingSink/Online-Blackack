7b4080c7df379d7afb32de7c80de51f4701a6304
Spelling Sink <daniel.dong8462@gmail.com>
2022-06-22 04:15:17 +1000

    first commit

---
+++ public/main.js
@@ -0,0 +1,115 @@
+var playerID = -1
+var disclaimerClicked = false
+function startgame()
+{
+    socket.emit('start')
+    document.getElementById('btnStart').value = 'Restart'
+}
+
+socket.on('updateDeck',(decklength) => {
+    document.getElementById('deckcount').innerHTML = decklength;
+})
+
+function getCardUI(card)
+{
+    var el = document.createElement('div')
+    var icon = ''
+    if (card.Suit == 'Hearts')
+    icon='&hearts;'
+    else if (card.Suit == 'Spades')
+    icon = '&spades;'
+    else if (card.Suit == 'Diamonds')
+    icon = '&diams;'
+    else
+    icon = '&clubs;'
+    
+    el.className = 'card'
+    el.innerHTML = card.Value + '<br/>' + icon
+    return el
+}
+socket.on('CreateUI',players => {
+    document.getElementById('players').innerHTML = ''
+    for(var i = 0; i < players.length; i++)
+    {
+    var div_player = document.createElement('div')
+    var div_playerid = document.createElement('div')
+    var div_hand = document.createElement('div')
+    var div_points = document.createElement('div')
+    var insertpoint = document.createElement('div')             //creates an element for each separate body of UI//
+    
+    div_points.className = 'points'
+    div_points.id = 'points_' + i
+    div_player.id = 'player_' + i
+    div_player.className = 'player'
+    div_hand.id = 'hand_' + i
+    insertpoint.id = 'insert_' + i                              //Assigns an id to each element//
+    
+    div_playerid.innerHTML = players[i].ID
+    div_player.appendChild(div_playerid)
+    div_player.appendChild(div_hand)
+    div_player.appendChild(div_points)
+    document.getElementById('players').appendChild(div_player)
+    div_player.appendChild(insertpoint)                           //Adds bodies as subsets of others to group them//
+    document.getElementById("status").style.display="block"
+    document.getElementById("turn").style.display="inline"
+    document.getElementById("name").style.display="block"
+    document.getElementById("name").innerHTML = 'You are: ' + socket.id
+    document.getElementById("status").innerHTML = ''
+    }
+})
+
+socket.on('PlayerLost',(PlayerLostID) => {
+    document.getElementById('status').innerHTML += 'Player: ' + PlayerLostID + ' LOST</br>'
+})
+
+socket.on('PlayerData',(players) =>{
+    for (var x = 0; x < players.length; x++)
+    {
+        var points = document.getElementById('insert_' + x)
+        points.innerHTML = players[x].Points
+        var hand = document.getElementById('hand_' + x)
+        hand.innerHTML = ''
+        for (var y = 0; y < players[x].Hand.length; y++)
+        {
+            hand.appendChild(getCardUI(players[x].Hand[y]))
+        }
+    }
+})
+
+function hitMe()
+{
+    socket.emit('hitMe')
+}
+function disclaimer()
+{
+    if (disclaimerClicked == false)
+    {
+        document.getElementById('disclaimer').innerHTML = "This online game of blackjack does not endorse gambling in any form, whether it be for material wealth, or for personal favours."
+        document.getElementById('disclaimer').innerHTML += "<br>Additionally, the user experience within this game does not accurately reflect how this game may be played in person, and as such,"
+        document.getElementById('disclaimer').innerHTML += "<br>please do not think that any data gathered here will be realistic or accurate"
+        disclaimerClicked = true
+        document.getElementById('disclaimer').style.display="block"
+    }
+    else if (disclaimerClicked == true)
+    {
+        document.getElementById('disclaimer').innerHTML = ''
+        document.getElementById('disclaimer').style.display="none"
+        disclaimerClicked = false
+    }
+}
+
+socket.on('PlayerTurn',(currentPlayer) =>{
+    document.getElementById('turn').innerHTML = 'It is ' + currentPlayer + "'s turn"
+})
+
+function stay()
+{
+    socket.emit('stay')
+}
+
+socket.on('end',(winner2) =>{
+    document.getElementById('status').style.display = 'block'
+    for (var x = 0; x < winner2.length; x++){
+        document.getElementById('status').innerHTML += '<br>Winner: Player ' + winner2[x] + '<br>'
+    }
+})
---
+++ server.js
@@ -0,0 +1,226 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+var playercount = 0
+var currentPlayer = 0
+var players = new Array()
+var cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+var suits = ["Diamonds", "Hearts", "Spades", "Clubs"]
+var deck = new Array()
+var haswon = false
+var ingame = false
+var currentPlayerNumber = 0
+app.use(express.static(__dirname + '/public'))
+
+function createDeck()
+{
+	var deck = new Array()
+
+	for(var i = 0; i < suits.length; i++)
+	{
+		for(var x = 0; x < cards.length; x++)
+		{
+			var card = {Value: cards[x], Suit: suits[i]}
+			deck.push(card)
+		}
+	}
+	return deck                                             //Creates a deck with cards for every value for every suit//
+}
+function shuffle()
+{
+	for (var i = 0; i < 1000; i++)
+	{
+		var location1 = Math.floor((Math.random() * deck.length))
+		var location2 = Math.floor((Math.random() * deck.length))
+		var tmp = deck[location1]                                               //Randomly swap the location of 2 cards for 1000 times//
+
+		deck[location1] = deck[location2]
+		deck[location2] = tmp
+	}
+}
+
+function createPlayers()
+{
+	players = new Array()
+  for (const [_, socket] of io.of("/").sockets) {
+    var hand = new Array()
+		var player = { Name: 'Player ' + socket.id, ID: socket.id, Points: 0, Hand: hand, Lost: false}
+		players.push(player)
+  }                        //Creates a player record with parameters in an array, iterating once for every player connected//
+}
+
+function dealHands()
+{
+    for(var i = 0; i < 2; i++)
+    {
+        for (var x = 0; x < players.length; x++)
+        {
+            var card = deck.pop()
+            players[x].Hand.push(card)             //Deals a card from the deck twice to every player hand//
+        }
+    }
+    for (var j = 0; j < players.length; j++)
+    {
+    updatePoints(j)                         //Updates the points of each player once//
+    }
+    updateDeck()
+}
+
+function updateDeck()
+{
+  io.emit('updateDeck',deck.length)
+}
+
+function updatePoints(player)
+{
+    var sum = 0
+    for (var x = 0; x < players[player].Hand.length; x++)
+    {
+        cardvalue = players[player].Hand[x].Value
+        if (cardvalue == 'J' || cardvalue == 'Q' || cardvalue == 'K')
+        {
+          cardvalue = 10      //Sets the card value to 10 for face cards//
+        }
+        else if (cardvalue == 'A')
+        {
+          cardvalue = 1
+        }
+        sum = sum + parseInt(cardvalue)
+    }
+    players[player].Points = sum
+    if (sum > 20){
+      stay(players[player].ID)
+    }
+}
+
+function hitMe(PlayerID)
+{
+  for (var x = 0; x < players.length; x++) {
+    if (players[x].ID == currentPlayer){
+      currentPlayerNumber = x
+    }
+  }
+  if (PlayerID == currentPlayer && ingame == true && players[currentPlayerNumber].Lost == false)
+    {
+      var card = deck.pop()
+      players[currentPlayerNumber].Hand.push(card)
+      updatePoints(currentPlayerNumber)
+      updateDeck()
+      check(currentPlayerNumber)
+      io.emit('PlayerData',(players))
+    }
+}
+
+function check(currentPlayerNumber)
+{
+    if (players[currentPlayerNumber].Points > 21)
+    {
+      io.emit('PlayerLost',players[currentPlayerNumber].ID)
+      players[currentPlayerNumber].Lost = true
+      stay(players[currentPlayerNumber].ID)
+    }
+}
+
+
+function startgame()
+{
+  
+  if (ingame == false)
+  {
+  ingame = true
+  haswon = false
+  deck = createDeck()
+  shuffle()
+  createPlayers()
+  dealHands()                             //Runs all the functions to set up a game//
+  var x = players.length - 1
+  currentPlayer = players[x].ID
+  io.emit('CreateUI',(players))
+  io.emit('PlayerData',(players))
+  io.emit('PlayerTurn',(currentPlayer))
+  }
+}
+
+function stay(PlayerID)
+{
+  for (var x = 0; x < players.length; x++) {
+    if (players[x].ID == currentPlayer){
+      currentPlayerNumber = x
+    }
+  }
+  if(haswon == false && ingame == true && currentPlayer == PlayerID)
+    {
+      if (currentPlayerNumber > 0) {
+        currentPlayerNumber--
+        currentPlayer = players[currentPlayerNumber].ID
+        io.emit('PlayerTurn',currentPlayer)
+        }
+      else if(currentPlayerNumber == 0) {
+          end()
+          haswon = true
+        }
+    }
+}
+
+function end()
+{
+  ingame = false
+  haswon = true
+  var winner1 = 0
+  var score = 0
+  var winner2 = new Array()
+
+    for(var i = 0; i < players.length; i++)
+    {
+        if (players[i].Points > score && players[i].Points < 22)
+        {
+          score = parseInt(players[i].Points)
+          winner1 = i
+        }
+    }
+    for(var j = 0; j < players.length; j++){
+        if (players[j].Points == score)
+        {
+          winner2.push(players[j].ID)
+        }
+    }
+    io.emit('end', winner2)
+}
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  playercount = playercount + 1
+  console.log('there are ' + playercount + ' players connected')
+  socket.on('start', () => {
+    startgame()
+  })
+  socket.on('hitMe',() => {
+    hitMe(socket.id)
+  })
+  socket.on('stay', () => {
+    stay(socket.id)
+  })
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    playercount = playercount - 1;
+    console.log('there are ' + playercount + ' players connected')
+    var existingPlayerID = 0
+    for (var i = 0; i < players.length; i++){
+      if (players[i].ID = socket.id){
+        existingPlayerID = socket.id
+      }
+    }
+    if (ingame == true && socket.id == existingPlayerID )
+    {
+      ingame = false
+      haswon = true
+    }
+  })
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
