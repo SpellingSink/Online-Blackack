0bcc716c4e43067f0674315d7bbc37604eb60725
SpellingSink <84759792+SpellingSink@users.noreply.github.com>
2022-06-22 08:20:59 +1000

    Update server.js

--- server.js
+++ server.js
@@ -221,6 +221,6 @@ io.on('connection', (socket) => {
   })
 });
 
-server.listen(3000 || process.env.PORT, () => {
+server.listen(process.env.PORT || 3000, () => {
   console.log('listening on *:3000');
 });
